29f4a9ccd555641af23336512067b8f2b7c28987
chbernat <bernat.christopher@gmail.com>
2019-06-21 22:50:42 +0200

    fixed auth

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -12,7 +12,7 @@ export class AuthGuard implements CanActivate {
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-        const privateKey = localStorage.getItem(PRIVATE_KEY);
+        const privateKey = localStorage.getItem('PRIVATE_KEY');
         if (!privateKey) {
             this.router.navigate(['connect']);
             return false;
