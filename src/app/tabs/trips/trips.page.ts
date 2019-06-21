2206d7a4a2a6b9eb89cf09a904bfc93cdd429e04
Mario Colombo <mario@what.digital>
2019-06-21 22:40:51 +0200

    added list of trips

--- src/app/tabs/trips/trips.page.ts
+++ src/app/tabs/trips/trips.page.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Web3Service} from '../../services/web3.service';
+import {PRIVATE_KEY} from '../../app.constants';
 
 @Component({
-  selector: 'app-trips',
-  templateUrl: './trips.page.html',
-  styleUrls: ['./trips.page.scss'],
+    selector: 'app-trips',
+    templateUrl: './trips.page.html',
+    styleUrls: ['./trips.page.scss'],
 })
 export class TripsPage implements OnInit {
 
-  constructor(private web3: Web3Service) { }
+    constructor(private web3: Web3Service) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        const privateKey = localStorage.getItem(PRIVATE_KEY);
 
+        this.web3.eth.personal.newAccount(privateKey, () => {
+            this.web3.eth.getAccounts().then((accounts) => {
+                const account = accounts[0];
+                this.web3.eth.defaultAccount = account;
+                const allTrips = this.web3.contract.methods.getTrips(
+                    account,
+                ).call({
+                    from: account,
+                }).then((result) => {
+                    // do something with the list of trips
+                    console.log(result);
+                });
+            });
+        });
+    }
 }
