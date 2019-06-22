2e3f3de09c60683e33ccb1a83658efa91278242f
chbernat <bernat.christopher@gmail.com>
2019-06-22 14:51:41 +0200

    ticket ver clean  up

--- src/app/ticket-verification/ticket-verification.page.ts
+++ src/app/ticket-verification/ticket-verification.page.ts
@@ -1,19 +1,19 @@
 import {Component, OnInit} from '@angular/core';
 import {Web3Service} from '../services/web3/web3.service';
-import Web3 from 'web3'
-import {Router} from '@angular/router'
+import Web3 from 'web3';
+import {Router} from '@angular/router';
 
-const l = console.log
+const l = console.log;
 
 @Component({
     selector: 'app-ticket-verification',
     templateUrl: './ticket-verification.page.html',
 })
 export class TicketVerificationPage implements OnInit {
-    web3: Web3
+    web3: Web3;
 
     constructor(private web3Service: Web3Service, private router: Router) {
-        this.web3 = web3Service.web3 as Web3
+        this.web3 = web3Service.web3 as Web3;
     }
 
     ngOnInit() {
@@ -21,18 +21,18 @@ export class TicketVerificationPage implements OnInit {
     }
 
     generateTicket() {
-        let web3: Web3 = this.web3Service.web3 as Web3
+        const web3: Web3 = this.web3Service.web3 as Web3;
         const privateKey = localStorage.getItem('PRIVATE_KEY');
-        let txHash = `tx hash 0x5077cb8488a849bc87a5996e730a07d3ddda29949ad6d7a5461ae0e6659876a5`
-        let sig_obj = web3.eth.accounts.sign(txHash, privateKey)
-        let sigString = JSON.stringify(sig_obj)
-        return sigString
+        const txHash = `tx hash 0x5077cb8488a849bc87a5996e730a07d3ddda29949ad6d7a5461ae0e6659876a5`;
+        const sigObj = web3.eth.accounts.sign(txHash, privateKey);
+        const sigString = JSON.stringify(sigObj);
+        return sigString;
     }
 
     onSuccessfulScanned(signatureObjString: string) {
-        let signatureObj: Object = JSON.parse(signatureObjString)
-        let publicKey: string = this.web3.eth.accounts.recover(signatureObj)
-        publicKey
+        const signatureObj: any = JSON.parse(signatureObjString);
+        const publicKey: string = this.web3.eth.accounts.recover(signatureObj);
+        // publicKey
     }
 
     onFailedScan(result: any) {
