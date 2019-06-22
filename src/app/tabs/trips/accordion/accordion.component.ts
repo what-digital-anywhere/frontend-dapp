e22cc33486cc67b1c98263cd5f79b40461331550
chbernat <bernat.christopher@gmail.com>
2019-06-22 21:01:59 +0200

    added accordion

---
+++ src/app/tabs/trips/accordion/accordion.component.ts
@@ -0,0 +1,34 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+@Component({
+    selector: 'app-accordion',
+    templateUrl: './accordion.component.html',
+    styleUrls: ['./accordion.component.scss']
+})
+export class AccordionComponent implements OnInit {
+
+    @Input() name: string;
+    @Input() description: string;
+    @Input() image: string;
+
+    @Output() change: EventEmitter<string> = new EventEmitter<string>();
+
+    public isMenuOpen = false;
+
+
+    constructor() {
+    }
+
+
+    ngOnInit() {
+    }
+
+    public toggleAccordion(): void {
+        this.isMenuOpen = !this.isMenuOpen;
+    }
+
+    public broadcastName(name: string): void {
+        this.change.emit(name);
+    }
+
+}
